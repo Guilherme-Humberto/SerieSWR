0ac6087a60b2c70ee012da0e5cab7a38e347fb6f
Guilherme Humberto <gbreu@outlook.com>
2020-10-11 22:31:39 -0300

    Update Fetcher.ts

--- src/hooks/Fetcher.ts
+++ src/hooks/Fetcher.ts
@@ -1,8 +1,8 @@
 import useSwr from 'swr'
 import api from '../services/api'
 
-export default function Fetcher<Config = any, Error = any>(url: string) {
-    const { data, error } = useSwr<Config, Error>(url, async url => {
+export default function Fetcher<Data = any, Error = any>(url: string) {
+    const { data, error } = useSwr<Data, Error>(url, async url => {
         const response = await api.get(url)
         const data = await response.data
 
